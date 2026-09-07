39401573d262bed02797e4aa10be538df0cdcc46
kadidala <91932187+kadidala@users.noreply.github.com>
2026-09-07 14:17:43 +0800

    Refactor password hashing functions to use optional salt
    Refactor hashPassword and comparePassword functions to accept an optional salt parameter, improving flexibility in password hashing and comparison.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,26 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function hashPassword(password: string): Promise<string> {
+export async function hashPassword(password: string, salt: string = process.env.AUTH_SECRET || ""): Promise<string> {
   const encoder = new TextEncoder()
-  let salt = ""
-  try {
-    const { getRequestContext } = await import("@cloudflare/next-on-pages")
-    const env = getRequestContext().env as unknown as Record<string, unknown>
-    if (typeof env?.AUTH_SECRET === "string") {
-      salt = env.AUTH_SECRET
-    } else {
-      salt = process.env.AUTH_SECRET || ""
-    }
-  } catch {
-    salt = process.env.AUTH_SECRET || ''
-  }
   const data = encoder.encode(password + salt)
   const hash = await crypto.subtle.digest('SHA-256', data)
   return btoa(String.fromCharCode(...new Uint8Array(hash)))
 }
 
-export async function comparePassword(password: string, hashedPassword: string): Promise<boolean> {
-  const hash = await hashPassword(password)
+export async function comparePassword(password: string, hashedPassword: string, salt: string = process.env.AUTH_SECRET || ""): Promise<boolean> {
+  const hash = await hashPassword(password, salt)
   return hash === hashedPassword
 }
